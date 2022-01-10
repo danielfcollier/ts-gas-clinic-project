f20297a0fa1c2eeb99600a281eefd631f9e50704
danielfcollier <danielfcollier@gmail.com>
2022-01-09 22:49:48 -0300

    Basic client-side form validation working.

--- va-app/src/client/js/booking/config/handlers.js
+++ va-app/src/client/js/booking/config/handlers.js
@@ -3,14 +3,37 @@ import searchBookingByInput from '../searchBookingByInput';
 
 import { buildTabCreateBooking, buildTabUpdateBooking } from '../../app/tabs';
 
+function validateCurrency() {
+  // eslint-disable-next-line
+  console.log('Ola');
+  const input = window.document.getElementById('consultationValue').value;
+  // eslint-disable-next-line
+  console.log({ input });
+
+  if (input === 1) {
+    return true;
+  }
+
+  return false;
+}
+
+function submitCreateBooking(e) {
+  // eslint-disable-next-line
+  console.log(e);
+}
+
 const bookingHandlers = {
   click: [
     { selector: '.confirmCancelBookingButton', callback: displayCancelConfirmation },
     { selector: '.createBookingButton', callback: buildTabCreateBooking },
     { selector: '.updateBookingButton', callback: buildTabUpdateBooking },
+    // { selector: '#submitButton', callback: submitCreateBooking },
     //   // { selector: '#updateBookingSaveButton', callback: editCustomer }
   ],
-  input: [{ selector: '#search-booking-input', callback: searchBookingByInput }],
+  input: [
+    { selector: '#search-booking-input', callback: searchBookingByInput },
+    { selector: '#consultationValue', callback: validateCurrency },
+  ],
 };
 
 export default bookingHandlers;
--- va-app/src/client/js/booking/createBookingByPatientId.js
+++ va-app/src/client/js/booking/createBookingByPatientId.js
@@ -8,6 +8,39 @@ import { setFormFields } from '../utils/form';
 const filterParams = ['fullName'];
 
 export default function createBookingByPatientId(params) {
+  const form = window.document.getElementById('needs-validation');
+
+  // Loop over them and prevent submission
+
+  form.addEventListener(
+    'submit',
+    (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      // form.classList.add('was-validated');
+      // eslint-disable-next-line
+      console.log({ msg: 'here 1', form });
+
+      for (let index = 0; index < form.elements.length; index++) {
+        const element = form.elements[index];
+        const { id, value } = element;
+        // eslint-disable-next-line
+        console.log({ id, value, element });
+        if (value === '2') {
+          element.classList.add('is-invalid');
+          return false;
+        }
+      };
+
+      // eslint-disable-next-line
+      console.log('Success process here...');
+
+      // form.classList.add('was-validated');
+      return false;
+    },
+    false,
+  );
+
   const patientData = getSessionDataById('patientBulkData', params.patientId);
 
   const filterValues = [];
