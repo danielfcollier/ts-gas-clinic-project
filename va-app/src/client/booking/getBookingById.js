34662ff28be2b17db1c9aea830d945cdba2c0d17
danielfcollier <danielfcollier@gmail.com>
2022-01-15 20:35:32 -0300

    Update booking basic structure.

--- va-app/src/client/booking/getBookingById.js
+++ va-app/src/client/booking/getBookingById.js
@@ -1,10 +1,52 @@
 import formFields from './config/formFields';
-import schema from './config/schema';
 
-import { getSessionDataById } from '../utils/sessionData';
+import checkFormValidation from '../form/checkFormValidation';
 import { setFormFields } from '../form/formFields';
+import { getSessionDataById } from '../utils/sessionData';
+import setFormEvents from '../form/setFormEvents';
+import validateForm from '../form/validateForm';
+
+import schema from './config/schema';
+import requiredField from '../form/requiredField';
+import selectOptions from './config/selectOptions';
+import paymentHandler from './config/paymentHandler';
+
+const validations = {
+  consultationTime: { validator: requiredField },
+  consultationDate: {
+    validator: (value) => value === 'R$ 1,00',
+    placeHolder: 'R$ 0,00',
+    mask: (value) => `R$ ${value},00`,
+    message: 'ola...'
+  },
+  consultationValue: {
+    validator: (value) => value === '003.212.781-27',
+    placeHolder: '000.000.000-00',
+    message: 'ola...'
+  }
+};
 
 export default function getBookingById(params) {
   const data = getSessionDataById('iBookingBulkData', params.id);
-  setFormFields([...formFields, 'id', 'fullName'], data, schema);
+  const patientFields = ['fullName'];
+  const newFormFields = [...formFields, ...patientFields];
+  setFormFields(newFormFields, data, selectOptions, schema);
+
+  const handlers = {
+    successHandler: {
+      callback: 1, // successHandlerCallback
+      action: 2, // serverFunction
+      params: 3 // params form server function
+    },
+    failureHandler: {
+      action: 1, // function
+      params: 2 // params
+    }
+  };
+
+  setFormEvents(validations, handlers);
+  paymentHandler();
+
+  //   validateForm(form, validations);
+  //   checkFormValidation(form, validations);
 }
