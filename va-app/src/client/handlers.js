5af71f73a36dac721b52671855b55565861d90db
danielfcollier <danielfcollier@gmail.com>
2022-01-15 10:12:53 -0300

    Refactoring handlers.

--- va-app/src/client/handlers.js
+++ va-app/src/client/handlers.js
@@ -5,16 +5,16 @@ import bookingHandlers from './booking/config/handlers';
 import patientHandlers from './patient/config/handlers';
 
 const appHandlers = {
-  click: [{ selector: '#cancelButton', callback: buildTabBooking }],
-  navClick: [{ selector: '.nav-link', callback: changeTab }],
+  click: [{ selector: '.cancelButton', callback: buildTabBooking }],
+  navClick: [{ selector: '.nav-link', callback: changeTab }]
 };
 
 const handlers = [appHandlers, bookingHandlers, patientHandlers];
 
-function clickEventHandler(e) {
+function handle(type, e) {
   for (const handler of handlers) {
-    if (handler.click) {
-      for (const event of handler.click) {
+    if (handler[type]) {
+      for (const event of handler[type]) {
         if (e.target.matches(event.selector)) {
           event.callback(e);
           break;
@@ -22,32 +22,21 @@ function clickEventHandler(e) {
       }
     }
   }
+}
+
+function clickEventHandler(e) {
+  const type = 'click';
+  handle(type, e);
 }
 
 function inputEventHandler(e) {
-  for (const handler of handlers) {
-    if (handler.input) {
-      for (const event of handler.input) {
-        if (e.target.matches(event.selector)) {
-          event.callback(e);
-          break;
-        }
-      }
-    }
-  }
+  const type = 'input';
+  handle(type, e);
 }
 
 function navClickEventHandler(e) {
-  for (const handler of handlers) {
-    if (handler.navClick) {
-      for (const event of handler.navClick) {
-        if (e.target.matches(event.selector)) {
-          event.callback(e);
-          break;
-        }
-      }
-    }
-  }
+  const type = 'navClick';
+  handle(type, e);
 }
 
 export { clickEventHandler, inputEventHandler, navClickEventHandler };
