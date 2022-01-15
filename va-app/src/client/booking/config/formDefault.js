0adbc1e8977e48f35ebc9c628d2245c3752a8fc0
danielfcollier <danielfcollier@gmail.com>
2022-01-15 10:15:02 -0300

    Better way to initialize the form.

--- va-app/src/client/booking/config/formDefault.js
+++ va-app/src/client/booking/config/formDefault.js
@@ -1,3 +1,29 @@
-const formDefault = ['Seguimento', 'teste 1', 'teste 2', 'Online', 'Beija-flor', '280', 'Padrão'];
+import selectOptions from './selectOptions';
 
-export default formDefault;
+const getConsultationMoment = (patientStatus) => {
+  return patientStatus ? selectOptions.consultationMoment[0] : selectOptions.consultationMoment[1];
+};
+const getConsultationValue = (consultationSet, consultationType) => 'R$ 240,00';
+
+export default function formDefault(patientStatus) {
+  const formValues = [];
+
+  const consultationMoment = getConsultationMoment(patientStatus);
+  const today = new Date();
+  const consultationDate = `dd/${(today.getMonth() + 1).toString().padStart(2, '0')}/${today.getFullYear()}`;
+  const consultationTime = '9h';
+  const consultationSet = selectOptions.consultationSet[0];
+  const paymentMethod = selectOptions.paymentMethod[0];
+  const consultationType = selectOptions.consultationType[0];
+  const consultationValue = getConsultationValue(consultationSet, consultationType);
+
+  formValues.push(consultationMoment);
+  formValues.push(consultationDate);
+  formValues.push(consultationTime);
+  formValues.push(consultationSet);
+  formValues.push(consultationType);
+  formValues.push(consultationValue);
+  formValues.push(paymentMethod);
+
+  return formValues;
+}
---
+++ va-app/src/client/booking/config/selectOptions.js
@@ -0,0 +1,8 @@
+const selectOptions = {
+  consultationMoment: ['Seguimento', '1ª Consulta'],
+  consultationSet: ['Online', 'Presencial', 'DIU', 'Emergência'],
+  consultationType: ['Beija-flor', 'Girassol', 'Artemísia'],
+  paymentMethod: ['Link de Pagamento', 'Autorizada', 'Sem Cobrança', 'Conta Bancária']
+};
+
+export default selectOptions;
